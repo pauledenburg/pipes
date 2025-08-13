ac3a7618c4ea5064ea0c7500f3dd807d4f20d288
pauledenburg <pauledenburg@users.noreply.github.com>
2025-08-13 18:06:11 +0000

    Fix styling

--- tests/migrations/0000_00_00_000000_create_package_test_tables.php
+++ tests/migrations/0000_00_00_000000_create_package_test_tables.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-// @codingStandardsIgnoreLine  
+// @codingStandardsIgnoreLine
 class CreatePackageTestTables_0000_00_00_000000 extends Migration
 {
     /**
